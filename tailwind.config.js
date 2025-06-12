b2411fcf9b67081d90b138d05eefb8f705be9aaf
Likho Zibi <425318@myuwc.ac.za>
2025-06-12 11:30:27 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: "class", // for dark mode toggle
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
